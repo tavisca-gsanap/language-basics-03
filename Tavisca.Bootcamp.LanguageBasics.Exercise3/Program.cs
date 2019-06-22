821665a246a4bc5a3ec5933bd507fae7828d1e5d
Govind Sanap <gsanap@tavisca.in>
2019-06-22 17:05:02 +0530

    Written program please check!

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
@@ -37,11 +38,248 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"Diet plan = [{string.Join(", ", dietPlans)}]");
             Console.WriteLine(result);
         }
+        static int[] frequency(int[] a, int x)
+        {
+            //int count = 0;
+            IList<int> b = new List<int>();
+            int k = 0;
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] == x) 
+                    b.Add(i);
+
+            return b.ToArray();
+        }
 
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            int[] cal = new int[protein.Length];
+            int[] op = new int[dietPlans.Length];
+            int[] p = frequency( protein,protein.Min());
+            int[] p1 = frequency(protein, protein.Max());
+            int[] c = frequency(carbs, carbs.Min());
+            int[] c1 = frequency(carbs, carbs.Max());
+            int[] f = frequency(fat, fat.Min());
+            int[] f1 = frequency(fat, fat.Max());
+            for (int i = 0; i < protein.Length; i++)
+            {
+                cal[i] = 9 * fat[i] + 5 * (protein[i] + carbs[i]);
+            }
+            int[] t = frequency(cal, cal.Min());
+            int[] t1 = frequency(cal, cal.Max());
+            for (int i = 0; i < dietPlans.Length; i++)
+            {
+                char[] diet = dietPlans[i].ToCharArray();
+                int j = 0;
+                IList<int> l = new List<int>();
+                bool fl = false;
+                while (j < diet.Length)
+                {
+
+                    if (diet[j] == 'p')
+                    {
+                        if (p.Length > 1)
+                        {
+                            foreach (int k in p) l.Add(k);
+                            fl = true;
+                            j++;
+                        }
+                        else
+                        {
+                            if (fl && !l.Contains(p[0]))
+                            {
+                                op[i] = l[0];
+                                j++;
+                            }
+                            else
+                            {
+                                op[i] = p[0];
+                                break;
+                            }
+                        }
+                    }
+                    else if (diet[j] == 'P')
+                    {
+                        if (p1.Length > 1)
+                        {
+                            j++;
+                            foreach (int k in p1) l.Add(k);
+                            fl = true;
+                        }
+                        else
+                        {
+                            if (fl && !l.Contains(p1[0]))
+                            {
+                                op[i] = l[0];
+                                j++;
+                            }
+                            else
+                            {
+                                op[i] = p1[0];
+                                break;
+                            }
+                        }
+                    }
+                    else if (diet[j] == 'c')
+                    {
+                        if (c.Length > 1)
+                        {
+                            j++;
+                            foreach (int k in c) l.Add(k);
+                            fl = true;
+                        }
+                        else
+                        {
+                            if (fl && !l.Contains(c[0]))
+                            {
+                                op[i] = l[0];
+                                j++;
+                            }
+                            else
+                            {
+                                op[i] = c[0];
+                                break;
+                            }
+                        }
+                    }
+                    else if (diet[j] == 'C')
+                    {
+                        if (c1.Length > 1)
+                        {
+                            j++;
+                            foreach (int k in c1) l.Add(k);
+                            fl = true;
+                        }
+                        else
+                        {
+                            if (fl && !l.Contains(c1[0]))
+                            {
+                                op[i] = l[0];
+                                j++;
+                            }
+                            else
+                            {
+                                op[i] = c1[0];
+                                break;
+                            }
+                        }
+                    }
+                    else if (diet[j] == 'f')
+                    {
+                        if (f.Length > 1)
+                        {
+                            j++;
+                            foreach (int k in f) l.Add(k);
+                            fl = true;
+                        }
+                        else
+                        {
+                            if (fl && !l.Contains(f[0]))
+                            {
+                                op[i] = l[0];
+                                j++;
+                            }
+                            else
+                            {
+                                op[i] = f[0];
+                                break;
+                            }
+                        }
+                    }
+                    else if (diet[j] == 'F')
+                    {
+                        if (f1.Length > 1)
+                        {
+                            j++;
+                            foreach (int k in f1) l.Add(k);
+                            fl = true;
+                        }
+                        else
+                        {
+                            if (fl && !l.Contains(f1[0]))
+                            {
+                                op[i] = l[0];
+                                j++;
+                            }
+                            else
+                            {
+                                op[i] = f1[0];
+                                break;
+                            }
+                        }
+                    }
+                    else if (diet[j] == 't')
+                    {
+                        if (t.Length > 1)
+                        {
+                            j++;
+                            foreach (int k in t) l.Add(k);
+                            fl = true;
+                        }
+                        else
+                        {
+                            if (fl && !l.Contains(t[0]))
+                            {
+                                op[i] = l[0];
+                                j++;
+                            }
+                            else
+                            {
+                                op[i] = t[0];
+                                break;
+                            }
+                        }
+                    }
+                    else if (diet[j] == 'T')
+                    {
+                        if (t1.Length > 1)
+                        {
+                            j++;
+                            foreach (int k in t1) l.Add(k);
+                            fl = true;
+                        }
+                        else
+                        {
+                            if (fl && !l.Contains(t1[0]))
+                            {
+                                op[i] = l[0];
+                                j++;
+                            }
+                            else
+                            {
+                                op[i] = t1[0];
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        op[i] = 0;
+                    }
+                }
+            }
+            /*
+            foreach (int i in cal) Console.Write(i+" ");
+            Console.WriteLine();
+            foreach (int i in p) Console.Write(i);
+            Console.WriteLine();
+            foreach (int i in p1) Console.Write(i);
+            Console.WriteLine();
+            foreach (int i in c) Console.Write(i);
+            Console.WriteLine();
+            foreach (int i in c1) Console.Write(i);
+            Console.WriteLine();
+            foreach (int i in f) Console.Write(i);
+            Console.WriteLine();
+            foreach (int i in f1) Console.Write(i);
+            Console.WriteLine();
+            foreach (int i in t) Console.Write(i);
+            Console.WriteLine();
+            foreach (int i in t1) Console.Write(i);
+            Console.WriteLine();
+            
+            foreach (int i in op) Console.Write(i);*/
+            return op;
         }
     }
 }
